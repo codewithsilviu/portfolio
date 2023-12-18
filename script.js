389f448a99f7c63cb9df951b34613f026af3bc9c
sivimerfu <silviumerfu@gmail.com>
2023-12-18 14:32:21 +0200

    Media Queries

---
+++ script.js
@@ -0,0 +1,6 @@
+const collapsibles = document.querySelectorAll(".collapsibles");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
