8bad29c1fe775efd9e64917c7bb5efcf48480f16
sivimerfu <silviumerfu@gmail.com>
2023-12-18 15:26:08 +0200

    Small Fix

--- script.js
+++
@@ -1,6 +0,0 @@
-const collapsibles = document.querySelectorAll(".collapsibles");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
